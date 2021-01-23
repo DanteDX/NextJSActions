d2457a0bc8f4fabed6e0a6a45695c681cc21df7b
dantedxwolf <teacher.martin@gmail.com>
2021-01-23 07:51:53 +0600

    CSS modules added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Sample from "./Sample";
 
 function Local(){
 	return(
 		<div>
 			<h4>This is Local Component</h4>
 			<h1>This should be green of color</h1>
+			<Sample />
 		</div>
 	)
 }
